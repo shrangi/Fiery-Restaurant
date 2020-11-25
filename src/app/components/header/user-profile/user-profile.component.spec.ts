6a161d4c337e018b7b1e96baa90533ac07006e7c
shrangi <ss.darksiders@gmail.com>
2020-11-25 19:03:33 +0530

    error handling and authentication

--- src/app/components/header/user-profile/user-profile.component.spec.ts
+++ src/app/components/header/user-profile/user-profile.component.spec.ts
@@ -3,28 +3,9 @@ import { FormsModule } from '@angular/forms';
 import { LoginService } from '../../security/login/login.service';
 
 import { UserProfileComponent } from './user-profile.component';
-import {RouterTestingModule} from "@angular/router/testing";
-import {Router} from "@angular/router";
 
-import { ROUTES} from '../../../app.routes';
-import { HomeComponent } from '../../home/home.component';
-import { LoginComponent } from '../../security/login/login.component';
-import { RestaurantComponent } from '../../restaurants/restaurant/restaurant.component';
-import { RestaurantDetailComponent } from '../../restaurants/restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from '../../restaurants/restaurant-detail/menu/menu.component';
-import { ReviewsComponent } from '../../restaurants/restaurant-detail/reviews/reviews.component';
-import { RestaurantsComponent } from '../../restaurants/restaurants.component';
-
-import { OrderComponent } from '../../order/order.component';
-import { OrdersComponent } from '../../orders/orders.component';
-import { NotFoundComponent } from '../../not-found/not-found.component';
-import { InputComponent } from '../../shared/input/input.component';
-import { MenuItemComponent } from '../../restaurants/restaurant-detail/menu-item/menu-item.component';
-import { ShoppingCartComponent } from '../../restaurants/restaurant-detail/shopping-cart/shopping-cart.component';
-import { SharedModule } from '../../shared/shared.module';
-import { CardComponent } from '../../order/card/card.component';
-import { OrderModule } from '../../order/order.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
@@ -32,27 +13,12 @@ describe('UserProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ 
-        UserProfileComponent, 
-        HomeComponent,
-        LoginComponent,
-        RestaurantDetailComponent,
-        MenuComponent,
-        RestaurantComponent,
-        ReviewsComponent,
-        RestaurantsComponent,
-        OrdersComponent,
-        NotFoundComponent,
-        MenuItemComponent,
-        ShoppingCartComponent,
-
+      declarations: [ UserProfileComponent
       ],
       imports: [
         FormsModule,
-        OrderModule,
         HttpClientModule,
-        SharedModule.forRoot(),
-        RouterTestingModule.withRoutes(ROUTES)
+        RouterTestingModule
       ],
       providers: [LoginService ],
 
